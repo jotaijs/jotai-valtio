4280345436cfdba3198f3988e8bd2398ef687cfe
daishi <daishi@axlight.com>
2024-05-24 23:45:49 +0900

    Module-first setup

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,17 +3,25 @@
 ## [Unreleased]
 
 ## [0.4.0] - 2023-10-07
+
 ### Added
+
 - feat: add api for returning a valtio proxyState from a jotai atom derived from another atom value #5
 
 ## [0.3.0] - 2023-03-04
+
 ### Added
+
 - feat: mark internal atoms as private
 
 ## [0.2.0] - 2023-01-31
+
 ### Added
+
 - Migrate to Jotai v2 API #1
 
 ## [0.1.0] - 2022-11-15
+
 ### Added
+
 - Initial release
--- src/mutableAtom.ts
+++ src/mutableAtom.ts
@@ -1,25 +1,25 @@
-import { atom } from 'jotai/vanilla'
-import { proxy, snapshot, subscribe } from 'valtio/vanilla'
+import { atom } from 'jotai/vanilla';
+import { proxy, snapshot, subscribe } from 'valtio/vanilla';
 
-type Wrapped<T> = { value: T }
+type Wrapped<T> = { value: T };
 
-type ProxyFn<T> = (obj: Wrapped<T>) => Wrapped<T>
+type ProxyFn<T> = (obj: Wrapped<T>) => Wrapped<T>;
 
 type Options<T> = {
-  proxyFn?: ProxyFn<T>
-}
+  proxyFn?: ProxyFn<T>;
+};
 
 export function mutableAtom<Value>(
   initialValue: Value,
-  options: Options<Value> = defaultOptions
+  options: Options<Value> = defaultOptions,
 ) {
-  const valueAtom = atom({ value: initialValue })
+  const valueAtom = atom({ value: initialValue });
 
   if (process.env.NODE_ENV !== 'production') {
-    valueAtom.debugPrivate = true
+    valueAtom.debugPrivate = true;
   }
 
-  const { proxyFn } = { ...defaultOptions, ...options }
+  const { proxyFn } = { ...defaultOptions, ...options };
 
   const storeAtom = atom<
     Store<Value>,
@@ -32,20 +32,20 @@ export function mutableAtom<Value>(
         getValue: () => setSelf({ type: 'getValue' }) as Value,
         setValue: (value: Value) =>
           setSelf({ type: 'setValue', payload: value }) as void,
-      }
-      return store
+      };
+      return store;
     },
     (get, set, action) => {
       if (action.type === 'setValue') {
-        set(valueAtom, { value: action.payload })
+        set(valueAtom, { value: action.payload });
       } else if (action.type === 'getValue') {
-        return get(valueAtom).value
+        return get(valueAtom).value;
       }
-    }
-  )
+    },
+  );
 
   if (process.env.NODE_ENV !== 'production') {
-    storeAtom.debugPrivate = true
+    storeAtom.debugPrivate = true;
   }
 
   /**
@@ -53,22 +53,22 @@ export function mutableAtom<Value>(
    */
   function onChange(getStore: () => Store<Value>) {
     return () => {
-      const { proxyState, getValue, setValue } = getStore()
-      const { value } = snapshot(proxyState)
+      const { proxyState, getValue, setValue } = getStore();
+      const { value } = snapshot(proxyState);
       if (!Object.is(value, getValue())) {
-        setValue(value as Awaited<Value>)
+        setValue(value as Awaited<Value>);
       }
-    }
+    };
   }
 
   /**
    * create the proxy state and subscribe to it
    */
   function createProxyState(getStore: () => Store<Value>) {
-    const proxyState = proxyFn({ value: initialValue })
+    const proxyState = proxyFn({ value: initialValue });
     // We never unsubscribe, but it's garbage collectable.
-    subscribe(proxyState, onChange(getStore), true)
-    return proxyState
+    subscribe(proxyState, onChange(getStore), true);
+    return proxyState;
   }
 
   /**
@@ -77,45 +77,45 @@ export function mutableAtom<Value>(
   function wrapProxyState(proxyState: ProxyState<Value>) {
     return new Proxy(proxyState, {
       get(target, property) {
-        return target[property as keyof ProxyState<Value>]
+        return target[property as keyof ProxyState<Value>];
       },
       set(target, property, value) {
         if (property === 'value') {
-          target[property] = value
-          return true
+          target[property] = value;
+          return true;
         }
-        return false
+        return false;
       },
-    })
+    });
   }
 
   /**
    * create an atom that returns the proxy state
    */
   const proxyEffectAtom = atom<ProxyState<Value>>((get) => {
-    get(valueAtom) // subscribe to value updates
-    const store = get(storeAtom)
-    return wrapProxyState(store.proxyState)
-  })
-  return proxyEffectAtom
+    get(valueAtom); // subscribe to value updates
+    const store = get(storeAtom);
+    return wrapProxyState(store.proxyState);
+  });
+  return proxyEffectAtom;
 }
 
 const defaultOptions = {
   proxyFn: proxy,
-}
+};
 
 type Store<Value> = {
-  proxyState: ProxyState<Value>
-  getValue: () => Value
-  setValue: (value: Value) => void
-}
+  proxyState: ProxyState<Value>;
+  getValue: () => Value;
+  setValue: (value: Value) => void;
+};
 
-export type ProxyState<Value> = { value: Value }
+export type ProxyState<Value> = { value: Value };
 
 type Action<Type extends string> = {
-  type: Type
-}
+  type: Type;
+};
 
 type ActionWithPayload<Type extends string, Payload> = Action<Type> & {
-  payload: Payload
-}
+  payload: Payload;
+};
