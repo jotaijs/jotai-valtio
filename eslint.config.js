7f90ae80f6173a67ba0ad6bcbf421c6adccdab4b
daishi <daishi@axlight.com>
2025-01-26 10:32:11 +0900

    flat config for react-hooks

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,9 @@
 import eslint from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
 import tseslint from 'typescript-eslint';
 import importPlugin from 'eslint-plugin-import';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
 import react from 'eslint-plugin-react';
-
-const compat = new FlatCompat();
+import reactHooks from 'eslint-plugin-react-hooks';
 
 export default tseslint.config(
   { ignores: ['dist/', 'website/'] },
@@ -15,7 +13,7 @@ export default tseslint.config(
   jsxA11y.flatConfigs.recommended,
   react.configs.flat.recommended,
   react.configs.flat['jsx-runtime'],
-  ...compat.extends('plugin:react-hooks/recommended'),
+  reactHooks.configs['recommended-latest'],
   {
     settings: {
       'import/resolver': { typescript: true },
